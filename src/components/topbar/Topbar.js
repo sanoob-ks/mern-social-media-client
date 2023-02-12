4bb186086d52108fe7a007526aa21cd7e744ba9e
sanoob-ks <sanoobks666@gmail.com>
2023-02-12 23:28:16 +0530

    topbar done

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,45 @@
+import './topbar.css'
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+function Topbar() {
+  return (
+    <div className='container'>
+        <div className="left">
+            <span className="logo">Social Media</span>
+        </div>
+        <div className="center">
+            <div className="searchbar">
+                <SearchIcon className='searchIcon'/>
+                <input placeholder='search for friends, post or video' className="searchInput" />
+            </div>
+        </div>
+        <div className="right">
+            <div className="topbarLinks">
+                <span className="topbarLink">Homepage</span>
+                <span className="topbarLink">Timeline</span>
+            </div>
+            <div className="topbarIcons">
+                <div className="topbarIconItem">
+                    <PersonIcon/>
+                    <span className="topbarIconBadge">2</span>
+                </div>
+                <div className="topbarIconItem">
+                    <ChatIcon/>
+                    <span className="topbarIconBadge">1</span>
+                </div>
+                <div className="topbarIconItem">
+                    <NotificationsIcon/>
+                    <span className="topbarIconBadge">5</span>
+                </div>
+            </div>
+            <img src="/assets/person/1.jpg" alt="" className="topbarImg" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Topbar
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import Topbar from "../../components/topbar/Topbar"
+
+
+function Home() {
+  return (
+    <div>
+      <Topbar/>
+    </div>
+  )
+}
+
+export default Home
