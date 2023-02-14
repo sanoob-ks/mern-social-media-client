0df52de4c3cba92d252c242c4d30535201cf918e
sanoob-ks <sanoobks666@gmail.com>
2023-02-14 23:48:28 +0530

    profile page completed

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,20 +1,77 @@
 import Online from '../online/Online'
 import './rightbar.css'
-import {Users} from '../../dummyData'
+import { Users } from '../../dummyData'
+
+function homeRightbar() {
+  return (
+    <>
+      <div className="bithdayContainer">
+        <img src="/assets/gift.png" alt="" className="birthdayImg" />
+        <span className="birthdayText"><b>Ameer E A </b>and<b> 3 other friends </b> have birthday today</span>
+      </div>
+      <img src="/assets/ad.png" alt="" className="righbarAdImg" />
+      <h4 className="rightbarTitle">Online Friends</h4>
+      <ul className="rightbarFriendList">
+        {Users.map(u => <Online user={u} />)}
+      </ul>
+    </>
+  )
+}
+
+const ProfileRightbar=()=>{
+  return(
+    <>
+      <h4 className='rightbarTitle'>User Information</h4>
+      <div className="rightbarInfo">
+        <div className="rihgtbarInfoItem">
+          <span className="rightabarInfoKey">city:</span>
+          <span className="rightabarInfoValue">Kochi</span>
+        </div>
+        <div className="rihgtbarInfoItem">
+          <span className="rightabarInfoKey">From:</span>
+          <span className="rightabarInfoValue">Kerala</span>
+        </div>
+        <div className="rihgtbarInfoItem">
+          <span className="rightabarInfoKey">Relationship:</span>
+          <span className="rightabarInfoValue">Single</span>
+        </div>
+        </div>
+        <h4 className='rightbarTitle'>User Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src="/assets/person/1.jpg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="/assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="/assets/person/1.jpg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="/assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="/assets/person/1.jpg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img src="/assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Mubashir B K</span>
+          </div>
+        </div>
+    </>
+  )
+}
 
 function Rightbar() {
   return (
     <div className='rightbar'>
       <div className="rightbarWrapper">
-        <div className="bithdayContainer">
-          <img src="/assets/gift.png" alt="" className="birthdayImg" />
-          <span className="birthdayText"><b>Ameer E A </b>and<b> 3 other friends </b> have birthday today</span>
-        </div>
-        <img src="/assets/ad.png" alt="" className="righbarAdImg" />
-        <h4 className="rightbarTitle">Online Friends</h4>
-        <ul className="rightbarFriendList">
-          {Users.map(u=><Online user={u}/>)}
-        </ul>
+          <ProfileRightbar/>
       </div>
     </div>
   )
---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,34 @@
+import './profile.css'
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Topbar from "../../components/topbar/Topbar"
+
+function Profile() {
+  return (
+    <div>
+      <Topbar/>
+      <div className="profileContainer">
+        <Sidebar/>
+        <div className="profileRight">
+            <div className="profileRightTop">
+                <div className="profileImg">
+                    <img src="/assets/post/2.jpeg" alt="" className="profileCoverImg" />
+                    <img src="/assets/person/1.jpg" alt="" className="profileUserImg" />
+                </div>
+                <div className="profileInfo">
+                    <h4 className='profileInfoName'>Sanoob K S</h4>
+                    <span className="profileInfoDesc">hai my name is sanoob</span>
+                </div>
+            </div>
+            <div className="profileRightBottom">
+                <Feed/>
+                <Rightbar Profile={true}/>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
